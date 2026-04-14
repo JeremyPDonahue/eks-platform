aac77f0e1d4aefbde60e2b78eca0a41f4b188e0a
Jeremy Donahue <jeremypdonahue@gmail.com>
2026-04-14 07:59:46 -0400

    Updated tags on NAT gateways

--- infra/modules/vpc/gateways.tf
+++ infra/modules/vpc/gateways.tf
@@ -1,3 +1,4 @@
+# INTERNET GATEWAY CONFIGURATION
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
@@ -7,22 +8,23 @@ resource "aws_internet_gateway" "this" {
 }
 
 # NAT GATEWAY CONFIGURATION
-resource "aws_eip" "eip" {
-  count  = 3
+resource "aws_eip" "nat" {
+  count  = var.single_nat_gateway ? 1 : length(local.azs)
   domain = "vpc"
 
+  tags = merge(local.common_tags, {
+    Name = "${var.cluster_name}-nat-eip-${count.index}"
+  })
 }
 
-resource "aws_nat_gateway" "nat" {
-  vpc_id = aws_vpc.this.id
-
-  count         = 3
-  allocation_id = aws_eip.eip[count.index].id
+resource "aws_nat_gateway" "this" {
+  count         = var.single_nat_gateway ? 1 : length(local.azs)
+  allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
-  tags = {
-    Name      = "${var.cluster_name}-${subnet_id}-nat"
-    ManagedBy = "terraform"
-  }
+  tags = merge(local.common_tags, {
+    Name = "${var.cluster_name}-nat-${count.index}"
+  })
 
+  depends_on = [aws_internet_gateway.this]
 }
