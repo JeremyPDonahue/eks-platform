88d6a5fa2ed4c5877d38f80243312da3f6661338
Jeremy Donahue <jeremypdonahue@gmail.com>
2026-04-14 07:56:59 -0400

    Updated tags section on vpc

--- infra/modules/vpc/vpc.tf
+++ infra/modules/vpc/vpc.tf
@@ -1,12 +1,9 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
+  enable_dns_hostnames = true # Required for EKS node registration
   enable_dns_support   = true
 
-  tags = {
-    Name        = "${var.cluster_name}-vpc"
-    Cluster     = var.cluster_name
-    Environment = var.environment
-    ManagedBy   = "terraform"
-  }
+  tags = merge(local.common_tags, {
+    Name = "${var.cluster_name}-vpc"
+  })
 }
